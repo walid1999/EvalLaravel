965aaccc5ac5abf9a8ba9f5805bebbba7ef2a8f7
walid1999 <56828930+walid1999@users.noreply.github.com>
2021-01-28 09:47:46 +0100

    creation base +header+footer + style css

---
+++ resources/views/base.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Esl Crew</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' href='assets/css/style.css'>
+    <script src="https://kit.fontawesome.com/45109b19e5.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Poppins&display=swap" rel="stylesheet">
+    
+</head>
+<body>
+
+@include('header')
+
+@yield('content')
+
+@include('footer')
+
+</body>
+</html>
---
+++ resources/views/header.blade.php
@@ -0,0 +1,35 @@
+<div class="contenu">
+    <header >
+
+		<h2>ESL CREW</h2>
+        
+		<nav>
+			<ul>
+				<li>
+					<a href="home.php">Accueil</a>
+                </li>
+                
+				<li>
+					<a href="boutique.php">Boutique</a>
+                </li>
+                
+				<li>
+					<a href="sav.php">Service client</a>
+                </li>
+
+                <li>
+                    <a href="panier.php">
+						<i class="fas fa-shopping-basket"></i>
+                    </a>
+                </li>
+            </ul>           
+        </nav>
+            <ul>
+				<li>
+                 	<a class="connexion" href="connexion.php"  >
+					 	Connexion
+				 		<i class="fas fa-sign-in-alt"></i>
+                 	</a> 
+				</li>
+            </ul>
+	</header>
