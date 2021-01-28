897c30aa1106358c01a3a148215e8c3fecbc3d06
walid1999 <56828930+walid1999@users.noreply.github.com>
2021-01-28 09:30:49 +0100

    creation de la base de donnée

---
+++ database/migrations/2021_01_28_082504_create_utilisateurs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUtilisateursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('utilisateurs', function (Blueprint $table) {
+            $table->id('id_utilisateur');
+            $table->string('email')->unique();
+            $table->string('pseudo');
+            $table->string('mot_de_passe');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('utilisateurs');
+    }
+}
